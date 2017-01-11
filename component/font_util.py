123890b29c1820565f44f0988acbf616270cbf60
liye <liye@qiyi.com>
2017-01-12 02:54:47 +0800

    2017-01-12

--- component/font_util.py
+++ component/font_util.py
@@ -29,7 +29,7 @@ def transfer_font_to_img(font_text, font_size, back_x, back_y, is_bold):
     offset_y = (back_y - font_y)/2
 
     # image with white back-ground color, (255, 255, 255) is white
-    back_img = Image.new("RGB", (back_x, back_y), (220, 220, 220))
+    back_img = Image.new("RGB", (back_x, back_y), (255, 255, 255))
     draw_brush = ImageDraw.Draw(back_img)
     draw_brush.text((offset_x, offset_y), font_text, font=font, fill="#000000")
     del draw_brush
